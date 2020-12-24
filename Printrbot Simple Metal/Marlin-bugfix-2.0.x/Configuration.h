c34386e0780dbfb2f14f1263a7228806909a2f73
JD Wallace <jd@jdwallace.com>
2020-12-23 22:57:23 -0800

    Recalibrated print area.

--- Printrbot Simple Metal/Marlin-bugfix-2.0.x/Configuration.h
+++ Printrbot Simple Metal/Marlin-bugfix-2.0.x/Configuration.h
@@ -1170,7 +1170,7 @@
 
 // The size of the print bed
 //#define X_BED_SIZE 152 // Printrbot standard print bed
-#define X_BED_SIZE 254   // Printrbot XL print bed
+#define X_BED_SIZE 252   // Printrbot XL print bed
 #define Y_BED_SIZE 152
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
@@ -1179,7 +1179,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 135    // Printrbot default is 152. This is too high
+#define Z_MAX_POS 155
 
 /**
  * Software Endstops
